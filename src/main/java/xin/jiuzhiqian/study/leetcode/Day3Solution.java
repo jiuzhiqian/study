a3076352944a0f442da37e9ad4c976d141eb9171
zhoufeng <zhoufeng@ludashi.com>
2020-08-28 09:23:24 +0800

    commit day3

--- src/main/java/xin/jiuzhiqian/study/leetcode/Day3Solution.java
+++ src/main/java/xin/jiuzhiqian/study/leetcode/Day3Solution.java
@@ -1,12 +1,55 @@
 package xin.jiuzhiqian.study.leetcode;
 
+import org.apache.logging.log4j.util.Strings;
+
 /**
  * @author : zhou
  */
 public class Day3Solution {
     public static void main(String[] args) {
         Day3Solution solution = new Day3Solution();
-
+        String s = "a dsfds ";
+        System.out.println(solution.lengthOfLastWord(s));
     }
 
+    public int lengthOfLastWord(String s) {
+        int len = s.length();
+        int lastIndex = len, firstIndex = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (firstIndex != 0) {
+                    return lastIndex - firstIndex;
+                } else {
+                    lastIndex = i;
+                }
+            } else {
+                firstIndex = i;
+            }
+        }
+        return lastIndex - firstIndex;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum = 0;
+        for (int num : nums) {
+            if (sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            res = Math.max(res, sum);
+        }
+        return res;
+    }
+
+    /**
+     * 垃圾题目
+     *
+     * @param n ll
+     * @return String
+     */
+    public String countAndSay(int n) {
+        return "" + n;
+    }
 }
